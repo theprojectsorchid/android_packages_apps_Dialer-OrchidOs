73cdcaaf50a0e65b6c33193fb53312ef31dde263
maxwen <max.weninger@gmail.com>
2017-11-25 13:27:51 +0100

    Dialer: unify calling account display and selection
    always also show carrier
    
    Change-Id: I98ea023dac8d59c1cd602775a82b6e6047acb9d1

--- Android.mk
+++ Android.mk
@@ -352,7 +352,7 @@ endif
 
 # End Bug: 37077388
 
-LOCAL_SDK_VERSION := system_current
+#LOCAL_SDK_VERSION := system_current
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := Dialer
 LOCAL_CERTIFICATE := shared
